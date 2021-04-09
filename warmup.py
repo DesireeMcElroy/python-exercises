5a08a5c846362e28e3187c631c329b2c8385a9d1
Desiree_McElroy <desireeashley90@gmail.com>
2021-04-09 11:43:16 -0700

    warm up exercises

---
+++ warmup.py
@@ -0,0 +1,101 @@
+# # Python String, List, Dictionary Warmup Exercises
+
+
+# # Write the code that operates on a single string containing the make and model of a 
+# vehicle. 
+# # The first part of the string before the space is the make The last part of the string 
+# after 
+# # the space is the model We can assume that the strings will only have one space. Assume 
+# the input string is completely lower-cased.
+
+# # Example inputs:
+
+# truck = "toyota tacoma"
+# sedan = "hyundai sonata"
+# sports_car = "lambourgini diablo"
+
+
+# # Exercise #1
+# # Write the code to take a string and produce a dictionary out of that string such that the output 
+# # looks like the following: Some thoughts:
+
+# # You'll need a way to get the first part of the string and a way to get the second part of the string
+# # Feel free to make new variables/data types in between the string and the output dictionary
+# # Input truck = "toyota tacoma"
+
+# Make the make and model into a list, then assign a variable to make and a 
+# variable to model
+
+make_and_model = 'toyota tacoma'
+make = make_and_model.split()[0]
+model = make_and_model.split()[1]
+
+print(make_and_model)
+print(make)
+print(model)
+
+truck = {
+    'make': make,
+    'model': model
+}
+
+print(truck)
+
+# Output
+
+# truck = {
+#     "make": "toyota",
+#     "model": "tacoma"
+# }
+
+
+
+# Exercise #2
+# Write the code that takes a dictionary containing make/model for a vehicle and capitalizes the 
+# first character of the make and the model:
+
+for element in truck:
+    truck['make'] = truck['make'].title()
+    truck['model'] = truck['model'].title()
+print(truck)
+
+
+
+# Input
+
+# truck = {
+#     "make": "toyota",
+#     "model": "tacoma"
+# }
+# Output
+
+# truck = {
+#     "make": "Toyota",
+#     "model": "Tacoma"
+# }
+
+
+
+
+# Exercise #3
+# Create a list of 3 dictionaries where each dictionary represents a vehicle, as above Write the code 
+# that operates on that list of dictionaries in order to uppercase the entire make and model values.
+
+for element in truck:
+    truck['make'] = truck['make'].upper()
+    truck['model'] = truck['model'].upper()
+print(truck)
+
+
+# Input
+
+# truck = {
+#     "make": "Toyota",
+#     "model": "Tacoma"
+# }
+# Output
+
+# truck = {
+#     "make": "TOYOTA",
+#     "model": "TACOMA"
+# }
