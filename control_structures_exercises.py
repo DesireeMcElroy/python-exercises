c445089305a44cb1779eb34d7e96b3818573f64a
Desiree_McElroy <desireeashley90@gmail.com>
2021-04-08 17:13:52 -0700

    adding initial exercises

--- control_structures_exercises.py
+++ control_structures_exercises.py
@@ -125,6 +125,8 @@ for n in range(1,10):
     print(str(n)*n)
 
 
+
+
 # Prompt the user for an odd number between 1 and 50. Use a loop and a break 
 # statement to continue prompting the user if they enter invalid input. 
 # (Hint: use the isdigit method on strings to determine this). 
